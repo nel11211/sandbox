bd1ddfa6ca3748af88dee5a64d8f784fd7b41b21
Nels Danielson <ndanielson@esri.com>
2025-04-21 17:47:08 -0700

    add peer deps

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import "@arcgis/charts-components/dist/components/arcgis-chart";
 
 document.querySelector('#app').innerHTML = `
   <div>

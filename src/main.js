5e33e3d5217c3af8dd63784ac2d021b15aa1a2fc
Nels Danielson <ndanielson@esri.com>
2025-04-21 18:49:53 -0700

    simplify

--- src/main.js
+++ src/main.js
@@ -1,26 +1,2 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-import "@arcgis/charts-components/dist/components/arcgis-chart";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-    <arcgis-chart></arcgis-chart>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+import "@arcgis/charts-components/components/arcgis-chart";
